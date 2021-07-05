8574b784618896fe8a8200cb999de50f2bd1192c
ogunsolu007 <63315246+ogunsolu007@users.noreply.github.com>
2021-07-06 00:23:23 +0100

    updated services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 /*===== Menu Show & Hidden ===== */
 const navMenu = document.getElementById("nav-menu")
-const navToggle= document.getElementById("nav-toggle")
+const navToggle = document.getElementById("nav-toggle")
 const navClose = document.getElementById("nav-close")
 
 
-if(navToggle){
-    navToggle.addEventListener("click", ()=>{
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu")
     })
 }
 
-if(navClose){
-    navClose.addEventListener("click", ()=>{
+if (navClose) {
+    navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
     })
 }
@@ -20,7 +20,7 @@ if(navClose){
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -32,18 +32,18 @@ const skillsContent = document.getElementsByClassName("skills__content")
 const skillsHeader = document.querySelectorAll(".skills__header")
 
 
-function toggleSkills(){
+function toggleSkills() {
     let itemClass = this.parentNode.className
 
-    for(i = 0; i< skillsContent.length; i++){
+    for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = "skills__content skills__close"
     }
-    if(itemClass === "skills__content skills__close"){
+    if (itemClass === "skills__content skills__close") {
         this.parentNode.className = "skills__content skills__open"
     }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((el) => {
     el.addEventListener("click", toggleSkills)
 })
 
@@ -52,17 +52,42 @@ const tabs = document.querySelectorAll("[data-target")
 const tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", ()=>{
+    tab.addEventListener("click", () => {
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tabContent =>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove("qualification__active")
         })
         target.classList.add("qualification__active")
 
-        tabs.forEach(tab =>{
+        tabs.forEach(tab => {
             tab.classList.remove("qualification__active")
         })
         tab.classList.add("qualification__active")
     })
 })
+
+/*================ Services ============ */
+
+const modalViews = document.querySelectorAll(".services__modal")
+const modalBtns = document.querySelectorAll(".services__button")
+ const modalCloses = document.querySelectorAll(".services__modal-close")
+
+ let modal = function(modalClick){
+     modalViews[modalClick].classList.add("active-modal")
+ }
+
+ modalBtns.forEach((modalBtn, i) => {
+     modalBtn.addEventListener("click", ()=>{
+         modal(i)
+     })
+ })
+
+ modalCloses.forEach((modalClose) =>{
+     modalClose.addEventListener("click", ()=>{
+         modalViews.forEach((modalView) =>{
+             modalView.classList.remove("active-modal")
+         })
+     })
+ })
+
